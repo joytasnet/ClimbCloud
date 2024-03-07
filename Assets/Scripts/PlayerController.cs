ff5216f35e811f628f1791690cb99db0d65f3612
mjpurin <mjpurin@gmail.com>
2024-03-07 14:12:52 +0900

    goal判定追加

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -39,4 +39,9 @@ public class PlayerController : MonoBehaviour
         this.animator.speed = speedx / 2.0f;
         
     }
+    void OnTriggerEnter2D(Collider2D collision) {
+        Debug.Log("ゴール");
+        
+    }
+
 }
